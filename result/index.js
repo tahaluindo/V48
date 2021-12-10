1699f90e0a49c0412b971c7050219a1fb775cf94
tahaluindo <94370774+tahaluindo@users.noreply.github.com>
2021-12-10 13:22:17 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ result/index.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+// new function
+function readfile(file) {
+  return JSON.parse(fs.readFileSync(file));
+}
+
+function pickRandom(list) {
+  return list[Math.floor(Math.random() * list.length)];
+}
+
+// -- file from result json
+const _verdad = readfile('./result/random/verdad.json');
+const _reto = readfile('./result/random/reto.json');
+
+// -- get results
+const verdad = () => {
+ return pickRandom(_truth);
+};
+
+const reto = () => {
+ return pickRandom(_dare);
+};
+
+
+
+// -- result download
+const { pinterest } = require('./download/pinterest_img');
+const { igDl } = require('./download/instagramPost_dl');
+const { Ttdl } = require('./download/tiktokPost_dl');
+const { yta, ytv } = require('./download/yt_download');
+// -- result search
+const { playstore } = require('./search/playstore');
+
+// information
+const { namaninja, purba, blackpill, typewriter, sans, castle } =  require('./informasi/text_generator');
+
+module.exports = {
+  verdad,
+  reto,
+  namaninja,
+  purba,
+  blackpill, 
+  typewriter, 
+  sans, 
+  castle,
+  pinterest,
+  playstore,
+  igDl,
+  Ttdl,
+  yta,
+  ytv
+};
